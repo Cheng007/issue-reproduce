d2271a6f0659fe3ae435f8b39b255d23a2bf330e
xiecheng <xiecheng@xiechengdeMacBook-Pro.local>
2019-03-20 17:56:06 +0800

    reproduce

---
+++ src/views/folder/Test.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>This is an test page</h1>
+    <div class="img"></div>
+  </div>
+</template>
+<style>
+.img {
+  background: url(./../../assets/logo.png);
+}
+</style>
+
